ca3349bdada37a49c2e3c1c98abeb1fdeb702f81
shifu9523 <acevedofksamu17@gmail.com>
2026-02-26 13:24:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,4 +5,5 @@ from PIL import Image
 image= Image.open("dim.jpeg")
 st.image(image,caption="Deportivo Independiente Medellin")
 texto=st.text_input("Ingresa texto","Texto inicial")
+st.write(texto)
 
