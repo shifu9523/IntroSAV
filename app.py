81350b5f0f5e56cda9017b10aa40422aaa5e1bca
shifu9523 <acevedofksamu17@gmail.com>
2026-02-26 13:23:45 -0500

    Add text input field to app

--- app.py
+++ app.py
@@ -4,4 +4,5 @@ st.header("Esta es mi pagina de presentación")
 from PIL import Image
 image= Image.open("dim.jpeg")
 st.image(image,caption="Deportivo Independiente Medellin")
+texto=st.text_input("Ingresa texto","Texto inicial")
 
