3e3b0077e33aad988093ab109c44d7fb6acfd03d
shifu9523 <acevedofksamu17@gmail.com>
2026-02-26 13:25:37 -0500

    Fix text output format in app.py

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ from PIL import Image
 image= Image.open("dim.jpeg")
 st.image(image,caption="Deportivo Independiente Medellin")
 texto=st.text_input("Ingresa texto","Texto inicial")
-st.write("El texto que has escrito es",texto)
+st.write("El texto que has escrito es:",texto)
 
